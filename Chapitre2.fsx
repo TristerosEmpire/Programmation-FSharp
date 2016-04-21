a39ca1197f9b74fb5f2f091f88ebaf544bef532c
TristeroEmpire <tristeros.empire@protonmail.com>
2016-04-21 17:37:07 +0200

    Premier Commit
     - ajout du readme.md
     - ajout du fichier complet Chapitreé.fsx

---
+++ Chapitre2.fsx
@@ -0,0 +1,49 @@
+﻿open System
+
+# Découverte de Liste
+
+let proches x = [
+    yield x-1;
+    yield x;
+    yield x+1;
+    ]
+
+# opérateurs d'agrégats avec List
+List.map (function x -> x*x) (proches 3)
+
+let insertVirgule acc item = acc+ ", " + item;;
+List.reduce insertVirgule ["Jean";"Jacques";"Pierre";"Jeanne";"Michelle";"Benoit"]
+
+let somme acc i = acc + i
+List.fold somme 0 [1;2;3]
+
+let compteVoyelles (str : string) =
+    let carListe = List.ofSeq str
+
+    let accumule (As,Es,Is,Os,Us) caractere = 
+        match caractere with
+        | 'a' -> (As+1,Es,Is,Os,Us)
+        | 'e' -> (As,Es+1,Is,Os,Us)
+        | 'i' -> (As,Es,Is+1,Os,Us)
+        | 'o' -> (As,Es,Is,Os+1,Us)
+        | 'u' -> (As,Es,Is,Os,Us+1)
+        | _ -> (As,Es,Is,Os,Us)
+    
+    List.fold accumule (0,0,0,0,0) carListe
+
+
+# OPTION
+let isInteger str = 
+    let succes, rslt = Int32.TryParse(str)
+
+    if succes then Some(rslt)
+    else None
+
+let recupNegatif liste =
+    let listeFiltree = List.filter (fun x -> x < 0) liste
+    if List.length listeFiltree > 0 then Some(listeFiltree)
+    else None
+
+let neg = recupNegatif [1;-2; -3; 4; 5;-8; 9];
+printfn "Résultat de l'extraction = %A" (Option.get neg)
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Découvertes et essais en FSharp
+
+Les différents codes sources présentés ici sont des reprises et des essais concluants en F# sous Linux à l'aide de MonoDevelop issus de l'excellent ouvrage de Chris Smith paru aux éditions O'Reilly __"Programming F# 3.0"__, 2<sup>ème</sup> édition.
