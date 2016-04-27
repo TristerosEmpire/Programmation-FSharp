e7f32ae953c85b3526b466af124394776d5f1705
TristeroEmpire <tristeros.empire@protonmail.com>
2016-04-27 10:41:05 +0200

    Amélioration du code

--- Chapitre2.fsx
+++ Chapitre2.fsx
@@ -1,6 +1,6 @@
 ﻿open System
 
-# Découverte de Liste
+// Découverte de Liste
 
 let proches x = [
     yield x-1;
@@ -8,7 +8,7 @@ let proches x = [
     yield x+1;
     ]
 
-# opérateurs d'agrégats avec List
+// opérateurs d'agrégats avec List
 List.map (function x -> x*x) (proches 3)
 
 let insertVirgule acc item = acc+ ", " + item;;
@@ -20,7 +20,7 @@ List.fold somme 0 [1;2;3]
 let compteVoyelles (str : string) =
     let carListe = List.ofSeq str
 
-    let accumule (As,Es,Is,Os,Us) caractere = 
+    let accumule (As,Es,Is,Os,Us) caractere =
         match caractere with
         | 'a' -> (As+1,Es,Is,Os,Us)
         | 'e' -> (As,Es+1,Is,Os,Us)
@@ -28,12 +28,12 @@ let compteVoyelles (str : string) =
         | 'o' -> (As,Es,Is,Os+1,Us)
         | 'u' -> (As,Es,Is,Os,Us+1)
         | _ -> (As,Es,Is,Os,Us)
-    
+
     List.fold accumule (0,0,0,0,0) carListe
 
 
-# OPTION
-let isInteger str = 
+// OPTION
+let isInteger str =
     let succes, rslt = Int32.TryParse(str)
 
     if succes then Some(rslt)
@@ -41,9 +41,9 @@ let isInteger str =
 
 let recupNegatif liste =
     let listeFiltree = List.filter (fun x -> x < 0) liste
-    if List.length listeFiltree > 0 then Some(listeFiltree)
+    if not (List.isEmpty listeFiltree) then Some listeFiltree
+    // ancienne version : if List.length listeFiltree > 0 then Some(listeFiltree)
     else None
 
 let neg = recupNegatif [1;-2; -3; 4; 5;-8; 9];
 printfn "Résultat de l'extraction = %A" (Option.get neg)
-
