a50bec0e08b9eb31f4d5e35c5e7287372b8698ff
TristeroEmpire <tristeros.empire@protonmail.com>
2016-05-19 22:13:24 +0200

    Chapitre 5 :         - création du fichier FSX         - ajout du premier code : union discriminée et redéfinition de           ToString ().

---
+++ Chapitre5.fsx
@@ -0,0 +1,21 @@
+﻿// PROGRAMMATION ORIENTEE OBJET
+open System
+open System.IO
+
+// Présentation de System.Object
+// surcharge de ToString ()
+
+type Ponctuation =
+    | Virgule
+    | Point
+    | PointInterrogation
+    | PointExclamation
+    override this.ToString () =
+        match this with
+        | Virgule               -> "Virgule (,)"
+        | Point                 -> "Point (.)"
+        | PointInterrogation    -> "PointInterrogation (?)"
+        | PointExclamation      -> "PointExclamation (!)"
+
+let x = Virgule
+x.ToString ()
