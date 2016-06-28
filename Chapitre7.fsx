678989ff2a6bdfa78178cc7faf26ca54773eb4ff
TristeroEmpire <tristeros.empire@protonmail.com>
2016-06-28 11:15:21 +0200

    Chapitre 7 : ajout des active patterns à cas partiels.

--- Chapitre7.fsx
+++ Chapitre7.fsx
@@ -17,3 +17,36 @@ let determineType (chemin:string) =
     | ExtensionFichier ".gif" -> printfn "Fichier image."
     | ExtensionFichier inconnu -> printfn "Format de fichier inconnu : %s" inconnu
 
+// cas partiels
+// 1er exemple : réécriture du cas simple qui génère une exception en cas partiel
+let (|ToInteger|_|) x = 
+    let succes, rslt = Int32.TryParse x
+    if succes then Some(rslt)
+    else None
+
+let isFour x = 
+    match x with
+    | ToInteger 4 -> true
+    | _       -> false 
+
+//2nd exemple (avec refactorisation du code original)
+
+let checkValue (succes, resultat) =
+    if succes then Some resultat
+    else None
+
+let (|ToBool|_|) x = 
+    checkValue (Boolean.TryParse x)
+
+let (|ToInt|_|) x =
+    checkValue (Int32.TryParse x)
+
+let (|ToFloat|_|) x =
+    checkValue (Double.TryParse x)
+
+let convertionString s =
+    match s with
+    | ToBool  resultat1 -> printfn "%s est un booléen ayant pour valeur %b" s resultat1
+    | ToInt   resultat2 -> printfn "%s est un booléen ayant pour valeur %d" s resultat2
+    | ToFloat resultat3 -> printfn "%s est un booléen ayant pour valeur %f" s resultat3
+    | _                 -> printfn "%s est d'un type inconnu" s
