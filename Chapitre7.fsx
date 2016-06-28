14d7e4dbbf8950ad1b0612e5aad3a0a9ec5baf9d
TristeroEmpire <tristeros.empire@protonmail.com>
2016-06-28 10:43:57 +0200

    Création du Chapitre 7 et autres modifs - ajout du code pour les Active Patterns à cas unique/simple - modification du readme.md

---
+++ Chapitre7.fsx
@@ -0,0 +1,19 @@
+﻿// PROGRAMMATION FONCTIONNELLE APPLIQUEE
+open System
+open System.IO
+
+// ACTIVE PATTERNS  
+// Cas simple :
+
+let (|ExtensionFichier|) chemin = Path.GetExtension chemin
+
+let determineType (chemin:string) =
+    match chemin with
+    // sans AP:
+    | chemin when  Path.GetExtension chemin = ".txt" -> printfn "Fichier texte."
+    // avec AP
+    | ExtensionFichier ".jpg"
+    | ExtensionFichier ".png"
+    | ExtensionFichier ".gif" -> printfn "Fichier image."
+    | ExtensionFichier inconnu -> printfn "Format de fichier inconnu : %s" inconnu
+
