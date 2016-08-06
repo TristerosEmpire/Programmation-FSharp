a9057ce6b7450e6f6885f09eff162c050291f327
TristeroEmpire <tristeros.empire@protonmail.com>
2016-08-06 10:10:41 +0200

    # Découvertes et essais en FSharp
    Les différents codes sources présentés ici sont des reprises et des essais concluants en F# sous Linux à l'aide de MonoDevelop issus de l'excellent ouvrage de Chris Smith paru aux éditions O'Reilly __"Programming F# 3.0"__, 2<sup>ème</sup> édition (ISBN : 978-1-449-32029-4).
    
    - :warning: aucun code pour le chapitre 1
    - :ok: chapitre 2 (*Chapitre2.fsx*)
    - :ok: chapitre 3 (*Chapitre3.fsx*)
    - :ok: chapitre 4 (*Chapitre4.fsx*)
    - :ok: chapitre 5 (*Chapitre5.fsx*, *ClasseEtSignature.fs*, *ClasseEtSignature.fsi*)
    - :ok: chapitre 6 (*Chapitre6.fsx*)
    - :ok: chapitre7 (*Chapitre7.fsx*)
    - :warning: chapitre 8 : *en cours*

--- Chapitre8.fsx
+++ Chapitre8.fsx
@@ -1,3 +1,20 @@
 ﻿// POO APPLIQUEE
 open System
 
+// Surcharge des opérateurs
+[<Measure>]
+type ML
+
+type Bouteille(volume: float<ML>) =
+    new() = new Bouteille(0.0<ML>)
+
+    member this.Volume = volume
+
+    override this.ToString() = sprintf "%.1f ml" (float this.Volume)
+
+    static member (+) (bg: Bouteille, bd: Bouteille ) =
+        new Bouteille(bg.Volume + bd.Volume)
+
+    static member (-) (bg: Bouteille, bd: Bouteille) = 
+        new Bouteille(bg.Volume-bd.Volume)
+    
