b3d0790c66427459d5a34f0a01d43a6b54835f9b
TristeroEmpire <tristeros.empire@protonmail.com>
2016-08-06 10:22:57 +0200

    Chapitre 8 : présentation de la surcharge de opérateurs Mise à jour du readme.md

--- Chapitre8.fsx
+++ Chapitre8.fsx
@@ -17,4 +17,13 @@ type Bouteille(volume: float<ML>) =
 
     static member (-) (bg: Bouteille, bd: Bouteille) = 
         new Bouteille(bg.Volume-bd.Volume)
-    
+
+    static member (~-) (b: Bouteille) = 
+        new Bouteille(b.Volume * -1.0<1>)
+
+    static member (+) (bg:Bouteille, bd:float<ML>) =
+        new Bouteille(bg.Volume+bd)
+
+    //nul besoin de spécifier comme ci-dessus le type
+    static member (-) (bg:Bouteille, bd) =
+        new Bouteille(bg.Volume-bd)
