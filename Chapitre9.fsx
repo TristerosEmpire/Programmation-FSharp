8835e28f89a8802e4a6e7ae1ac6f85435fe8a1c6
TristeroEmpire <tristeros.empire@protonmail.com>
2016-09-16 10:28:21 +0200

    Chapitre 9 : compétition entre threads avec 2 résolutions (non-bloquante et bloquante)

--- Chapitre9.fsx
+++ Chapitre9.fsx
@@ -30,3 +30,88 @@ let afficheNombre (max : obj) =
         printfn "%d" i
 // soit on utilise explicitement le délégué avec la nécessité de faire un boxing sur le type int
 ThreadPool.QueueUserWorkItem(new WaitCallback(afficheNombre), box 5)
+
+// Problèmes : compétitions / race conditions
+let sommeTableauErreur (tab: int[]) = 
+    let total = ref 0
+
+    let thread1Achevé = ref false
+
+    ThreadPool.QueueUserWorkItem(
+        fun _ -> for i = 0 to tab.Length / 2 - 1 do
+                    total := tab.[i] + !total
+                 thread1Achevé := true
+    ) |> ignore
+
+    let thread2Achevé = ref false
+
+    ThreadPool.QueueUserWorkItem(
+        fun _ -> for i = tab.Length  / 2 to tab.Length - 1 do
+                    total := tab.[i] + !total
+                 thread2Achevé := true
+    ) |> ignore
+
+    while !thread1Achevé = false || !thread2Achevé = false do
+        Thread.Sleep(0)
+
+    !total
+
+let millionUn = Array.create 1000000 1
+
+sommeTableauErreur millionUn
+
+// résolution sans blocage: 
+
+let sommeTableauNonBloquée (tab:int[]) =
+    let sstotal1 = ref 0
+    let sstotal2 = ref 0
+    let thread1Achevé = ref false
+
+    ThreadPool.QueueUserWorkItem(
+        fun _ -> for i = 0 to tab.Length / 2 - 1 do
+                    sstotal1 := tab.[i] + !sstotal1
+                 thread1Achevé := true
+    ) |> ignore
+
+    let thread2Achevé = ref false
+
+    ThreadPool.QueueUserWorkItem(
+        fun _ -> for i = tab.Length  / 2 to tab.Length - 1 do
+                    sstotal2 := tab.[i] + !sstotal2
+                 thread2Achevé := true
+    ) |> ignore
+
+    // attendre que les 2 threads aient fini leur travail respectif
+    while !thread1Achevé = false || !thread2Achevé = false do
+        Thread.Sleep(0)
+
+    !sstotal1 + !sstotal2
+
+sommeTableauNonBloquée millionUn
+sommeTableauNonBloquée millionUn
+
+// résolution avec la fonction lock : ('a -> (unit -> 'b) -> 'b) when 'a : not struct
+
+let sommeTableauBloqué (tab:int[]) =
+    let total = ref 0
+
+    let thread1Achevé = ref false
+    ThreadPool.QueueUserWorkItem(
+        fun _ -> for i = 0 to tab.Length / 2 - 1 do
+                    lock total (fun () -> total := tab.[i] + !total)
+                 thread1Achevé := true
+    ) |> ignore
+
+    let thread2Achevé = ref false
+    ThreadPool.QueueUserWorkItem(
+        fun _ -> for i = tab.Length / 2 to tab.Length - 1 do
+                    lock total (fun () -> total := tab.[i] + !total)
+                 thread2Achevé := true
+    ) |> ignore
+
+    while !thread1Achevé = false || !thread2Achevé = false do
+        Thread.Sleep(0)
+
+    !total
+
+sommeTableauBloqué millionUn
