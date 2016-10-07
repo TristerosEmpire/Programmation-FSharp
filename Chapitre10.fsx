d5a7c87fdfe5c8e5af4360a8e8ca7bc4f45b862f
TristeroEmpire <tristeros.empire@protonmail.com>
2016-10-07 09:41:24 +0200

    Chapitre 10 : colorisation en mode console

--- Chapitre10.fsx
+++ Chapitre10.fsx
@@ -6,7 +6,7 @@ open System.Windows.Forms
 open System.Drawing
 
 // Directives génériques
-let affiche_moi () = 
+let afficheMoi () =
     printfn "Je suis %s et je me trouve dans %s" __SOURCE_FILE__ __SOURCE_DIRECTORY__
     printfn "Chemin complet : %s" (Path.Combine [|__SOURCE_DIRECTORY__; __SOURCE_FILE__|] )
 
@@ -17,8 +17,8 @@ let affiche_moi () =
 #r "System.Drawing.dll"
 
 // on veut retourner un type seq<string * Drawing.Bitmap>
-let images chemin = 
-    Directory.GetFiles(chemin, "*.jpg") |> Seq.map (fun fichier -> 
+let images chemin =
+    Directory.GetFiles(chemin, "*.jpg") |> Seq.map (fun fichier ->
             Path.GetFileName(fichier), Bitmap.FromFile(fichier))
 
 // création d'un datagrid
@@ -29,3 +29,28 @@ dg.DataSource <- new List<_>(images (Environment.GetFolderPath(Environment.Speci
 let f = new Form()
 f.Controls.Add(dg)
 f.ShowDialog()
+
+// coloration en mode console
+let colorisation (couleur:ConsoleColor) texte =
+    // utilisation de la fonction Printf.kprintf de signature :
+    // (string -> 'a) -> Printf.StringFormat<'b,'a> -> 'b
+    Printf.kprintf (fun chaine -> let original = Console.ForegroundColor
+                                  Console.ForegroundColor <- couleur
+                                  Console.WriteLine chaine
+                                  Console.ForegroundColor <- original)
+                   texte
+
+colorisation ConsoleColor.Blue "hello en bleu."
+
+let rotationCouleurs =
+// aura pour signature :rotationCouleurs : seq<ConsoleColor>
+    // récupération des valeurs prédéfinies de ConsoleColor
+    let couleursPossibles = Enum.GetValues(typeof<ConsoleColor>)
+    // obligation de contraindre le type de retour : ConsoleColor sinon erreur d'ambiguité 
+    let couleurs (index:int) : ConsoleColor = enum (index % couleursPossibles.Length)
+    Seq.initInfinite couleurs
+
+// application de rotationCouleurs
+"Faire l'expérience d'un arc-en-ciel de possibilité !"
+|> Seq.zip rotationCouleurs
+|> Seq.iter (fun (couleur, caractere) -> colorisation couleur "%c" caractere) 
