999824c392c483e2c1f4fd7571f4ff8692a002cb
TristeroEmpire <tristeros.empire@protonmail.com>
2016-10-17 09:05:40 +0200

    Chapitre 11 : suppression du fichier et renvoi vers le dépôt GitHub de Chris Smith

--- Chapitre11.fsx
+++
@@ -1,3 +0,0 @@
-﻿// Traitement des données / Data Processing
-// Code source se trouve à l'adresse suivante :
-// https://github.com/achrissmith/Programming-FS-Examples/tree/master/Ch%2012%20-%20Data%20Processing
